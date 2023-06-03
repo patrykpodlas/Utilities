a1f6c631a3ae8d7da453a64ab940a2f4d80d373d
Patryk <patryk.podlas@outlook.com>
2023-06-03 10:13:12 +0100

    update

---
+++ code_signing/upload-files-to-blob.ps1
@@ -0,0 +1,23 @@
+Param (
+    [string]$NewFilesAndTheirHashesJson,
+    [string]$ServicePrincipalSecret,
+    [string]$StorageAccountResourceGroupName,
+    [string]$StorageAccount
+)
+
+$NewFilesAndTheirHashes = $NewFilesAndTheirHashesJson | ConvertFrom-Json
+Write-Output "--- Logging into storage account."
+$SecurePassword = ConvertTo-SecureString -String $ServicePrincipalSecret -AsPlainText -Force
+$PSCredential = New-Object System.Management.Automation.PSCredential($env:ServicePrincipalID, $SecurePassword)
+Connect-AzAccount -ServicePrincipal -Credential $PSCredential -Tenant $env:TenantID
+Write-Output "--- Logged into Azure."
+$Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
+# Set the hashes
+foreach ($File in $NewFilesAndTheirHashes) {
+    $Metadata = @{
+        SHA256 = $File.SHA256
+    }
+    Write-Output "--- Uploading file: $($File.Name) and setting metadata with hash: $($File.SHA256)."
+    Set-AzStorageBlobContent -Container $(destination-container) -File $File.Name -Metadata $Metadata -Force -Context $Context | Out-Null
+    Write-Output "--- Blob uploaded."
+}
