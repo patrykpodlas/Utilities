9a1f4177986b7a9275b0638073502c97330ddb69
Patryk <patryk.podlas@outlook.com>
2023-06-03 09:18:06 +0100

    update

---
+++ code_signing/get-list-of-signed-files.ps1
@@ -0,0 +1,17 @@
+Write-Output "--- Logging into storage account, to retrieve a list of already signed files."
+$SecurePassword = ConvertTo-SecureString -String $(service-principal-secret) -AsPlainText -Force
+$PSCredential = New-Object System.Management.Automation.PSCredential($env:ServicePrincipalID, $SecurePassword)
+Connect-AzAccount -ServicePrincipal -Credential $PSCredential -Tenant $env:TenantID
+Write-Output "--- Logged into Azure."
+Write-Output "--- Searching for existing signed files."
+$Context = $(Get-AzStorageAccount -ResourceGroupName $(storage-account-resource-group-name) -Name $(storage-account)).Context
+# Getting list of files in the blob, the object will also contain the existing SHA256 metadata.
+$ExistingFiles = Get-AzStorageBlob -Container $(destination-container) -Context $Context |
+Select-Object -Property @{Name = "Name"; Expression = { $_.Name.Split('/')[-1] } }, `
+                        @{Name = "SHA256"; Expression = { $_.BlobClient.GetProperties().Value.Metadata.SHA256 } }
+
+Write-Output "--- Existing files in the storage account:"
+$ExistingFiles | Format-Table Name, SHA256
+
+$ExistingFilesJson = ($ExistingFiles | ConvertTo-Json -Compress)
+Write-Host "##vso[task.setvariable variable=ExistingFilesJson;]$ExistingFilesJson"
