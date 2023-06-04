67ed9672e1675d63712b71a55cd44f1eaa4ed6da
Patryk <patryk.podlas@outlook.com>
2023-06-04 18:09:40 +0100

    update

--- code_signing/get-list-of-signed-files.ps1
+++ code_signing/get-list-of-signed-files.ps1
@@ -5,7 +5,9 @@ Param(
 )
 
 Write-Output "--- Searching for existing signed files."
-$Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
+$Context = Get-AzContext
+#$Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
+
 # Getting list of files in the blob, the object will also contain the existing SHA256 metadata.
 $ExistingFiles = Get-AzStorageBlob -Container $DestinationContainer -Context $Context |
 Select-Object -Property @{Name = "Name"; Expression = { $_.Name.Split('/')[-1] } }, `
