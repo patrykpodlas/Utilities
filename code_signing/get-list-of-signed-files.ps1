3d4a358dbc970c6b233a188a7fdda6ba65440feb
Patryk <patryk.podlas@outlook.com>
2023-06-04 10:09:50 +0100

    update

--- code_signing/get-list-of-signed-files.ps1
+++ code_signing/get-list-of-signed-files.ps1
@@ -8,7 +8,7 @@ Param(
 Write-Output "--- Logging into storage account, to retrieve a list of already signed files."
 $SecurePassword = ConvertTo-SecureString -String $ServicePrincipalSecret -AsPlainText -Force
 $PSCredential = New-Object System.Management.Automation.PSCredential($env:ServicePrincipalID, $SecurePassword)
-Get-Module -ListAvailable | Format-Table -AutoSize
+Import-Module Azure -RequiredVersion 2.1.0
 Import-Module -Name Az.Accounts
 Connect-AzAccount -ServicePrincipal -Credential $PSCredential -Tenant $env:TenantID
 Write-Output "--- Logged into Azure."
