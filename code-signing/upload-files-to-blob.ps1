03d13bdd65c1134c15a45a8065fbe9acf5b1c262
Patryk <patryk.podlas@outlook.com>
2023-06-05 09:04:48 +0100

    update

--- code-signing/upload-files-to-blob.ps1
+++ code-signing/upload-files-to-blob.ps1
@@ -9,11 +9,17 @@ $NewFilesAndTheirHashes = $env:NewFilesAndTheirHashesJson | ConvertFrom-Json
 
 $Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
 # Set the hashes
+$Results = @()
 foreach ($File in $NewFilesAndTheirHashes) {
     $Metadata = @{
         SHA256 = $File.SHA256
     }
-    Write-Output "--- Uploading file: $($File.Name) and setting metadata with hash: $($File.SHA256)."
     Set-AzStorageBlobContent -Container $DestinationContainer -File $File.Name -Metadata $Metadata -Force -Context $Context | Out-Null
-    Write-Output "--- Blob uploaded."
+    $Results += New-Object PSObject -Property @{
+        File   = $_.Name
+        Result = "Blob uploaded"
+        SHA256 = $_.SHA256
+    }
 }
+
+$Results | Select-Object -Property File, Result, SHA256 -AutoSize
