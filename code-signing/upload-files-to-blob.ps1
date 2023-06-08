4a8e519cf89cf0d9e00905c6e1c2a45a89c58a71
Patryk <patryk.podlas@outlook.com>
2023-06-08 09:20:02 +0100

    update

--- code-signing/upload-files-to-blob.ps1
+++ code-signing/upload-files-to-blob.ps1
@@ -11,13 +11,10 @@ $Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGrou
 # Upload to blob and set the hashes
 $Results = @()
 foreach ($File in $NewFilesAndTheirHashes) {
-    Write-Output $File.RelativePath
-    $BlobPrefix = Split-Path $File.RelativePathBlob -Parent
-    Write-Output $BlobPrefix
     $Metadata = @{
         SHA256 = $File.SHA256
     }
-    Set-AzStorageBlobContent -Container $DestinationContainer -File $File.RelativePath -Blob $BlobPrefix -Metadata $Metadata -Force -Context $Context | Out-Null
+    Set-AzStorageBlobContent -Container $DestinationContainer -File $File.RelativePath -Blob $File.RelativePathBlob -Metadata $Metadata -Force -Context $Context | Out-Null
     $Results += New-Object PSObject -Property @{
         RelativePathBlob = $File.RelativePathBlob
         Result           = "Blob uploaded"
