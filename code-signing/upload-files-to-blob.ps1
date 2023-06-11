95673d2e7aff3fc5e540abafac911a2f69b6ca9e
Patryk <patryk.podlas@outlook.com>
2023-06-11 11:15:54 +0100

    update

--- code-signing/upload-files-to-blob.ps1
+++ code-signing/upload-files-to-blob.ps1
@@ -2,10 +2,12 @@ Param (
     [string]$ServicePrincipalSecret,
     [string]$StorageAccountResourceGroupName,
     [string]$StorageAccount,
-    [string]$DestinationContainer
+    [string]$DestinationContainer,
+    [string]$ExecutablesContainer
 )
 
 $NewFilesAndTheirHashes = $env:NewFilesAndTheirHashesJson | ConvertFrom-Json
+$NewExecutablesAndTheirHashes = $env:NewExecutablesAndTheirHashesJson | ConvertFrom-Json
 
 $Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
 # Upload to blob and set the hashes
@@ -15,6 +17,24 @@ foreach ($File in $NewFilesAndTheirHashes) {
         SHA256 = $File.SHA256
     }
     Set-AzStorageBlobContent -Container $DestinationContainer -File $File.RelativePath -Blob $File.RelativePathBlob -Metadata $Metadata -Force -Context $Context | Out-Null
+    $Results += New-Object PSObject -Property @{
+        RelativePathBlob = $File.RelativePathBlob
+        Result           = "Blob uploaded"
+        SHA256           = $File.SHA256
+    }
+}
+
+$Results | Format-Table -Property RelativePathBlob, Result, SHA256 | Out-String -Width 200
+
+#Executables
+$Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
+# Upload to blob and set the hashes
+$Results = @()
+foreach ($File in $NewExecutablesAndTheirHashes) {
+    $Metadata = @{
+        SHA256 = $File.SHA256
+    }
+    Set-AzStorageBlobContent -Container $ExecutablesContainer -File $File.RelativePath -Blob $File.RelativePathBlob -Metadata $Metadata -Force -Context $Context | Out-Null
     $Results += New-Object PSObject -Property @{
         RelativePathBlob = $File.RelativePathBlob
         Result           = "Blob uploaded"
