b842197bf7eceaada87ca6ec095bb5c2aeaf34fd
Patryk <patryk.podlas@outlook.com>
2023-07-09 13:23:00 +0100

    update

--- code-signing/get-list-of-signed-files.ps1
+++ code-signing/get-list-of-signed-files.ps1
@@ -8,9 +8,8 @@ Param(
 Write-Output "--- Searching for existing signed files in the storage account."
 $Context = $(Get-AzStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccount).Context
 # Getting list of files in the blob, the object will also contain the existing SHA256 metadata.
-Write-Output "--- Searching for PowerShell scripts in the $DestinationContainer."
-$ExistingFiles = Get-AzStorageBlob -Container $DestinationContainer -Context $Context | Select-Object -Property Name, @{Name = "SimpleName"; Expression = { $_.Name.Split('/')[-1] } }, @{Name = "SHA256"; Expression = { $_.BlobClient.GetProperties().Value.Metadata.SHA256 } }, @{Name = "Extension"; Expression = { $_.Name.Split('.')[-1] } }
-Write-Output "--- Searching for executables in the $ExecutablesContainer."
+$ExistingFiles = @()
+$ExistingFiles += Get-AzStorageBlob -Container $DestinationContainer -Context $Context | Select-Object -Property Name, @{Name = "SimpleName"; Expression = { $_.Name.Split('/')[-1] } }, @{Name = "SHA256"; Expression = { $_.BlobClient.GetProperties().Value.Metadata.SHA256 } }, @{Name = "Extension"; Expression = { $_.Name.Split('.')[-1] } }
 $ExistingFiles += Get-AzStorageBlob -Container $ExecutablesContainer -Context $Context | Select-Object -Property Name, @{Name = "SimpleName"; Expression = { $_.Name.Split('/')[-1] } }, @{Name = "SHA256"; Expression = { $_.BlobClient.GetProperties().Value.Metadata.SHA256 } }, @{Name = "Extension"; Expression = { $_.Name.Split('.')[-1] } }
 
 Write-Output "--- Existing files in the storage account:"
